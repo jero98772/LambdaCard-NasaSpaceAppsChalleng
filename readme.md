c33139ac687068a489d5c12cb005b91c55155af2
jero98772 <jero98772@protonmail.com>
2022-09-30 19:28:59 -0500

    frist commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# labdaOrbit
+### features 
+### Screenshots
+### Installing
+**Download repositories**
+
+    git clone https://github.com/lambdaCard/labdaOrbit.git
+
+**Run:**  
+
+### Made for:
+#### Ethical purpose
+#### Non-ethical purpose
---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-"
+"""
+labdaOrbit - by lambdaCard
+"""
+from setuptools import setup, find_packages
+setup(
+	name='',
+	version='',
+	license='GPLv3',
+	author_email='',
+	author='lambdaOrbit',
+	description='',
+	url='',
+	packages=find_packages(),
+    install_requires=['Flask','pyrtlsdr'],
+    include_package_data=True,
+	)
